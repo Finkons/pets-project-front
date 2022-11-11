43a1ec9d4908939aa40791f7665beff85eaee1aa
Roman <g4vr0m@gmail.com>
2022-11-11 14:11:59 +0200

    add pet modal and backdrop component

---
+++ src/components/Modal/Backdrop/Backdrop.styled.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const BackdropLayer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${p => p.theme.colors.greyText};
+  backdrop-filter: blur(10px);
+  padding: 0 20px;
+  overflow: scroll;
+`;

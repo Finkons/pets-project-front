3e9b386c157b2dc642d8f5f6a6769f20c8cabdac
OksanaOlefirenko <oksana_olefirenko@ukr.net>
2022-11-12 21:40:27 +0200

    add files for auth

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from "components/AuthForm/RegisterForm";
+// import { Container } from './LoginPage.styled';
+
+const RegisterPage = () => {
+  return (
+    // <Container>
+    <RegisterForm />
+    // </Container>
+  );
+};
+
+export default RegisterPage;

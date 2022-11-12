2dfd980d74221d0b5b036ccf2f3948f53702389e
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-11-12 19:56:49 +0200

    Update NoticesCategoriesNav.jsx

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -3,16 +3,16 @@ import { Button, List, Li } from "./NoticesCategoriesNav.styled";
 export default function NoticesCategoriesNav() {
   return (
     <List>
-      <Li style={{ marginRight: 12 }}>
+      <Li>
         <Button to="/notices/sell">Sell</Button>
       </Li>
-      <Li style={{ marginRight: 12 }}>
+      <Li>
         <Button to="/notices/lost-found">lost/found</Button>
       </Li>
-      <Li style={{ marginRight: 12 }}>
+      <Li>
         <Button to="/notices/for-free">In good hands</Button>
       </Li>
-      <Li style={{ marginRight: 12 }}>
+      <Li>
         <Button to="/notices/favorite">Favorite ads</Button>
       </Li>
       <Li>

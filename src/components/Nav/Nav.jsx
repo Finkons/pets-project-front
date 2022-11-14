267a6404210bca33c1c35be30e6a565079dd581a
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-11-14 22:09:06 +0100

    test commit

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import { NavHeader,Link,Text } from "./Nav.styled";
 const Nav = () => {
   return (
       <NavHeader>
-    <Link  to="/news"> <Text>News</Text></Link>
+    <Link  to="/news"> <Text>NewsTEST</Text></Link>
     <Link  to="/notices/sell"> <Text>Find pet</Text></Link>
     <Link  to="/friends"> <Text>Our friend</Text> </Link>
       </NavHeader>

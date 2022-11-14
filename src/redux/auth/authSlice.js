2c1cbcd76febb27aac25a4f392c1ead9ed52e853
kyslynsky <kyslynsky@gmail.com>
2022-11-14 11:14:39 +0200

    add user id to redux state

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -4,14 +4,18 @@ import { authApi } from "./authApi";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
+    userId: null,
     name: null,
     email: null,
+    isLoggedIn: false,
     token: null,
   },
   extraReducers: builder => {
     builder.addMatcher(authApi.endpoints.register.matchFulfilled, (state, { payload: { user, token } }) => {
+      state.userId = user._id;
       state.name = user.name;
       state.email = user.email;
+      state.isLoggedIn = true;
       state.token = token;
     });
     builder.addMatcher(authApi.endpoints.login.matchFulfilled, (state, { payload: { user, token } }) => {

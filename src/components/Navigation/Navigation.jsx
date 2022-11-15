09b69f791376f3f95d1089f2b6617b15f8523609
Andrii Mykhailenko <91851050+Finkons@users.noreply.github.com>
2022-11-15 22:14:54 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import Nav from 'components/Nav/Nav';
+import Nav from "components/Nav/Nav";
 import AuthNav from "components/AuthNav/AuthNav";
 import UserNav from "components/UserNav/UserNav";
 import  authSelectors  from "redux/auth/authSelectors";

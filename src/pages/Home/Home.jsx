15481e14b21a30cd6b7d649cc753991df12cf770
Andrii Mykhailenko <91851050+Finkons@users.noreply.github.com>
2022-11-15 22:15:37 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
 import { Container,Title,HomeImage,Gradient,Heart } from "./Home.styled";
-import girl from "img/homePages-photos/desktop-girl-1x.png"
-import heart from "img/homePages-photos/desktop-heart-1x.png"
-import gradient from "img/homePages-photos/Frame.png"
-// import BurgerMenu from "components/BurgerMenu/BurgerMenu"
+import girl from "img/homePages-photos/desktop-girl-1x.png";
+import heart from "img/homePages-photos/desktop-heart-1x.png";
+import gradient from "img/homePages-photos/Frame.png";
+// import BurgerMenu from "components/BurgerMenu/BurgerMenu";
 
 
 const Home = () => {

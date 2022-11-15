8ec684321cfbd6d1ffd18ddac36cff4e08dcfacc
Andrii Mykhailenko <91851050+Finkons@users.noreply.github.com>
2022-11-15 22:14:19 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,8 @@
-import { useSelector } from 'react-redux';
-import Nav from 'components/Nav/Nav'
-import AuthNav from 'components/AuthNav/AuthNav';
-import UserNav from 'components/UserNav/UserNav';
-import  authSelectors  from 'redux/auth/authSelectors';
+import { useSelector } from "react-redux";
+import Nav from 'components/Nav/Nav';
+import AuthNav from "components/AuthNav/AuthNav";
+import UserNav from "components/UserNav/UserNav";
+import  authSelectors  from "redux/auth/authSelectors";
 
 const Navigation = () => {
 const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);

bebda8b3659297fce2f7d959311a0a4e039c17b9
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-11-16 17:20:46 +0200

    add routing by auth and some api

--- src/components/AddNoticeButton/AddNoticeButton.jsx
+++ src/components/AddNoticeButton/AddNoticeButton.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import { notifyWarning } from "helpers/toastNotifications";
 import { Div, Label, Button } from "./AddNoticeButton.styled";
+import authSelectors from "redux/auth/authSelectors";
+import { useSelector } from "react-redux";
 
 export default function AddNoticeButton() {
   const [expanded, setExpanded] = useState(false);
-  const [isLoggedIn] = useState(false);
+
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   const handleModalToggle = () => {
     if (!isLoggedIn) return notifyWarning("You need to log in to perform this action");
     setExpanded(!expanded);
+    console.log("log in");
   };
   return (
     <Div>

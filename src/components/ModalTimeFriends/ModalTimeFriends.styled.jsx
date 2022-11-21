55c5ecf34076fe7af24987a48e00819f3b48058f
Dmytro Khomenko <dmytro.khomenko.it@gmail.com>
2022-11-21 18:16:15 +0200

    update Modal

--- src/components/ModalTimeFriends/ModalTimeFriends.styled.jsx
+++ src/components/ModalTimeFriends/ModalTimeFriends.styled.jsx
@@ -1,35 +1,39 @@
-.modal {
+import styled from "@emotion/styled";
+
+export const Modal = styled.div`
+width: 295px;
+height: 287px;
+transform: translate(25%,-60%);
+display: flex;
+align-items: center;
+justify-content: center;
+opacity: 0;
+pointer-events: none;
+transition: 0.5s;
+@media ${p => p.theme.media.desktop} {
+  width: 295px;
+  height: 287px;
+  transform: translate(35%,-80%);
+}
+`
+
+
   /* height: 100vh;
   width: 100vw;
   position: fixed;
   top: 0;
   left: 0; */
-  width: 295px;
-  height: 287px;
-  transform: translate(35%,-80%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: 0;
-  pointer-events: none;
-  transition: 0.5s;
-}
-
-.modal.active {
-  opacity: 1;
-  pointer-events: all;
-}
-/* 
-.modal__content {
+ 
+  export const ModalContent = styled.div`
   padding: 12px;
   background: #ffffff;
   border: 1px solid #f59256;
   box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.25);
   border-radius: 8px;
   min-width: 96px;
-}
+  `
 
-.modal__text {
+  export const ModalText = styled.ul`
   font-size: 12px;
   list-style: none;
   text-decoration: none;
@@ -42,16 +46,16 @@
   font-weight: 500;
   font-family: sans-serif;
   line-height: 1.33;
-}
+  `
 
-.text__position {
+  export const TextPosition = styled.li`
   margin-bottom: 4px;
   display: flex;
   justify-content: flex-end;
-}
+  `
 
-.workspace__position {
+  export const WorkspacePosition = styled.span`
   display: flex;
   margin-left: 12px;
   justify-content: flex-end;
-} */
+  `

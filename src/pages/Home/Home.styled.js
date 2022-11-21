8598364d9382db67c64c7041780529a1df618aa5
AnnaZaluzhnaya <92920694+AnnaZaluzhnaya@users.noreply.github.com>
2022-11-21 23:43:29 +0100

    fixed HomeImg

--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -1,22 +1,31 @@
 import styled from "@emotion/styled";
+import gradient from "img/homePages-photos/Frame.png";
 
 export const Container = styled.div`
   position: relative;
   min-width: 320px;
   overflow: hidden;
   margin: 0 auto;
-
+  display: block;
+  width: 100%;
   @media ${p => p.theme.media.tablet} {
     min-height: 768px;
   }
   @media ${p => p.theme.media.desktop} {
-    width: 1280px;
+    display: flex;
+    min-width: 1280px;
+    background-image: url(${gradient});
+    // max-width: 100vw;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center bottom;
+    min-height: calc(100vh - 70px);
   }
 `;
 
 export const ContentContainer = styled.div`
   margin: 0 auto;
-  width: 320px;
+  // width: 320px;
   @media ${p => p.theme.media.tablet} {
     width: 768px;
     @media ${p => p.theme.media.desktop} {
@@ -25,7 +34,22 @@ export const ContentContainer = styled.div`
   }
 `;
 
+// export const ContentContainer = styled.div`
+//   // margin-top: 60px;
+//   position: absolute;
+//   margin: 0 auto;
+//   width: 320px;
+//   @media screen and (min-width: 768px) {
+//     font-size: 68px;
+//     line-height: 100px;
+//     width: 768px;
+//   }
+//   @media screen and (min-width: 1280px) {
+//     width: 1280px;
+// `;
+
 export const Title = styled.h1`
+  margin-top: 58px;
   margin-bottom: 58px;
   margin-left: 20px;
   font-family: "Manrope";
@@ -35,6 +59,10 @@ export const Title = styled.h1`
   line-height: 44px;
   color: black;
   align-items: center;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 44px;
   @media ${p => p.theme.media.tablet} {
     width: 588px;
     height: 200px;
@@ -44,37 +72,41 @@ export const Title = styled.h1`
     line-height: 100px;
   }
   @media ${p => p.theme.media.desktop} {
-    position: absolute;
+    // position: absolute;
     width: 588px;
     height: 200px;
-    left: 16px;
+    // left: 16px;
     top: 50px;
     font-size: 68px;
     line-height: 100px;
+    margin-top: 103px;
+    margin-left: 16px;
   }
 `;
 
 export const ImgContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
+  // position: absolute;
+  // width: 100%;
+  // height: 100%;
 `;
 
 export const HomeImage = styled.img`
   bottom: 0;
   width: 320px;
   @media ${p => p.theme.media.tablet} {
-    width: 100%;
     bottom: 0;
     width: 699px;
   }
   @media ${p => p.theme.media.desktop} {
     width: 624px;
-    left: 656px;
-    top: 0;
+    // left: 656px;
+    // top: 113px;
+    margin-top: 46px;
     z-index: 2;
     overflow: hidden;
-    margin-left: auto;
+    // margin-left: auto;
+    bottom: -200px;
+    min-height: calc(100vh - 70px);
   }
 `;
 
@@ -94,19 +126,19 @@ export const BackgroundImage = styled.img`
   }
 `;
 
-export const Gradient = styled.img`
-  display: none;
+// export const Gradient = styled.img`
+//   display: none;
 
-  @media ${p => p.theme.media.desktop} {
-    position: absolute;
-    display: flex;
-    z-index: -1;
-    width: 100%;
-    top: 230px;
-    bottom: 0;
-    repeat: no-repeat;
-  }
-`;
+//   @media ${p => p.theme.media.desktop} {
+//     position: absolute;
+//     display: flex;
+//     z-index: -1;
+//     width: 100%;
+//     top: 230px;
+//     bottom: 0;
+//     repeat: no-repeat;
+//   }
+// `;
 
 export const Heart = styled.img`
   display: none;
@@ -115,8 +147,8 @@ export const Heart = styled.img`
     position: absolute;
     width: 91.93px;
     height: 89px;
-    left: 700px;
-    top: 60px;
+    left: 712px;
+    top: 120px;
     z-index: -1;
   }
 `;

e18c08601743cba051de1ea162375b26fde7a9a7
Andrey <andrey@MacBook-Air-Andrey.local>
2022-11-23 18:02:12 +0000

    add home page style to mobile version

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,15 +7,15 @@ import heart from "img/homePages-photos/desktop-heart-1x.png";
 const Home = () => {
   return (
     <BackgroundImageFix>
-      <HomeImage>
-        <img src={girl} alt="homePhoto" />
-      </HomeImage>
+    <HomeImage>
+          <img src={girl} alt="homePhoto" />
+        </HomeImage>
+        <BackgroundImage src={background} alt="homeBack" />
+        {/* <Gradient src={gradient} alt="GradientTop" /> */}
       <Container>
         <ContentContainer>
           <Title>Take good care of your small pets</Title>
         </ContentContainer>
-        <BackgroundImage src={background} alt="homeBack" />
-        {/* <Gradient src={gradient} alt="GradientTop" /> */}
         <Heart src={heart} alt="heart" />
       </Container>
     </BackgroundImageFix>
